40e2489fb80434b4fcdd01f82c226c57dac43863
Victorienne Tiu <victoriennetiu@gmail.com>
2024-09-03 21:37:53 +0800

    Add personalized greeting feature

--- vi.c
+++ vi.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main(){ 
-    printf("Hello World!\n");
+
+    char user[50];
+
+    printf("Enter name: ");
+    gets(user);
+    
+    printf("Hello World %s\n", user);
     printf("This is Vi's Project");
 
     return 0; 
